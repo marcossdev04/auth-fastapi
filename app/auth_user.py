3d78b6ae911e87fec0d41fd3d0ca36286a5134d9
MRCDEV <126817362+marcossdev04@users.noreply.github.com>
2025-01-29 16:20:04 -0300

    User Login

--- app/auth_user.py
+++ app/auth_user.py
@@ -5,8 +5,14 @@ from passlib.context import CryptContext
 from fastapi.exceptions import HTTPException
 from sqlalchemy.exc import IntegrityError
 from fastapi import status
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from decouple import config
 
+SECRET_KEY = config('SECRET_KEY')
+ALGORITHM = config('ALGORITHM')
 crypt_context = CryptContext(schemes=["sha256_crypt"],)
+
 
 class UserUseCases:
     def __init__( self, db_session: Session):
@@ -25,3 +31,30 @@ class UserUseCases:
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Username already exists"
             )
+        
+    def user_login(self,user:User,expires_in:int = 30):
+        user_on_db = self.db_session.query(UserModel).filter_by(username = user.username).first()
+
+        if user_on_db is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid username or password"
+            )
+        if not crypt_context.verify(user.password, user_on_db.password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid username or password"
+            )
+        exp = datetime.now() + timedelta(minutes=expires_in)
+
+        payload = {
+            "sub": user.username,
+            "exp": exp
+        }
+        
+        token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+        
+        return {
+            "access_token": token,
+            "exp" : exp.isoformat()
+        }
--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from app.depends import get_db_session
 from app.auth_user import UserUseCases
 from app.schemas import User
-
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(prefix = '/user')
 
@@ -20,4 +20,20 @@ def user_register(
     return JSONResponse(
         content={'msg':'User registered'},
         status_code=status.HTTP_201_CREATED
+    )
+
+@router.post('/login')
+def user_login(
+    request_form_user: OAuth2PasswordRequestForm = Depends(),
+    db_session: Session = Depends(get_db_session),
+):
+    uc = UserUseCases(db_session = db_session)
+    user = User(
+        username=request_form_user.username,
+        password=request_form_user.password
+    )
+    auth_data = uc.user_login(user=user)
+    return JSONResponse(
+        content=auth_data,
+        status_code=status.HTTP_200_OK
     )
